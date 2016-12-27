294cd3b7d7b3370d1b33b3f78c1d41bdde7a65a3
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2016-12-27 13:07:51 +0100

    Work on ex 5

--- udacity_course/5_word2vec.py
+++ udacity_course/5_word2vec.py
@@ -14,6 +14,7 @@ from sklearn.manifold import TSNE
 
 url = 'http://mattmahoney.net/dc/'
 
+# download the data
 def maybe_download(filename, expected_bytes):
     """Download a file if not present, and make sure it's the right size."""
     if not os.path.exists(filename):
@@ -29,6 +30,7 @@ def maybe_download(filename, expected_bytes):
 
 filename = maybe_download('text8.zip', 31344016)
 
+# dezip it
 def read_data(filename):
     """Extract the first file enclosed in a zip file as a list of words"""
     with zipfile.ZipFile(filename) as f:
@@ -66,6 +68,10 @@ del words  # Hint to reduce memory.
 
 data_index = 0
 
+# batch size: number of examples in the batch
+# num_skips: number of times single word is repeated as a label
+# skip_window: length of window target for the word is chosen from
+# TODO does anything but num_skips = 2*skip_window make sense?
 def generate_batch(batch_size, num_skips, skip_window):
     global data_index
     assert batch_size % num_skips == 0
@@ -99,108 +105,128 @@ for num_skips, skip_window in [(2, 1), (4, 2)]:
     print('    batch:', [reverse_dictionary[bi] for bi in batch])
     print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
 
-batch_size = 128
-embedding_size = 128 # Dimension of the embedding vector.
-skip_window = 1 # How many words to consider left and right.
-num_skips = 2 # How many times to reuse an input to generate a label.
-# We pick a random validation set to sample nearest neighbors. here we limit the
-# validation samples to the words that have a low numeric ID, which by
-# construction are also the most frequent.
-valid_size = 16 # Random set of words to evaluate similarity on.
-valid_window = 100 # Only pick dev samples in the head of the distribution.
-valid_examples = np.array(random.sample(range(valid_window), valid_size))
-num_sampled = 64 # Number of negative examples to sample.
 
-graph = tf.Graph()
+def skip_word2vec():
 
-with graph.as_default(), tf.device('/cpu:0'):
+    batch_size = 128
+    embedding_size = 128 # Dimension of the embedding vector.
+    skip_window = 1 # How many words to consider left and right.
+    num_skips = 2 # How many times to reuse an input to generate a label.
+    # We pick a random validation set to sample nearest neighbors. here we limit the
+    # validation samples to the words that have a low numeric ID, which by
+    # construction are also the most frequent.
+    valid_size = 16 # Random set of words to evaluate similarity on.
+    valid_window = 100 # Only pick dev samples in the head of the distribution.
+    valid_examples = np.array(random.sample(range(valid_window), valid_size))
+    num_sampled = 64 # Number of negative examples to sample.
 
-    # Input data.
-    train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
-    train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-    valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+    graph = tf.Graph()
 
-    # Variables.
-    embeddings = tf.Variable(
-      tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    softmax_weights = tf.Variable(
-      tf.truncated_normal([vocabulary_size, embedding_size],
-                           stddev=1.0 / math.sqrt(embedding_size)))
-    softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
+    with graph.as_default(), tf.device('/cpu:0'):
 
-    # Model.
-    # Look up embeddings for inputs.
-    embed = tf.nn.embedding_lookup(embeddings, train_dataset)
-    # Compute the softmax loss, using a sample of the negative labels each time.
-    loss = tf.reduce_mean(
-      tf.nn.sampled_softmax_loss(softmax_weights, softmax_biases, embed,
-                                 train_labels, num_sampled, vocabulary_size))
+        # Input data.
+        train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
+        train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+        valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
-    # Optimizer.
-    # Note: The optimizer will optimize the softmax_weights AND the embeddings.
-    # This is because the embeddings are defined as a variable quantity and the
-    # optimizer's `minimize` method will by default modify all variable quantities
-    # that contribute to the tensor it is passed.
-    # See docs on `tf.train.Optimizer.minimize()` for more details.
-    optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+        # Variables.
+        embeddings = tf.Variable(
+          tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        softmax_weights = tf.Variable(
+          tf.truncated_normal([vocabulary_size, embedding_size],
+                               stddev=1.0 / math.sqrt(embedding_size)))
+        softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
-    # Compute the similarity between minibatch examples and all embeddings.
-    # We use the cosine distance:
-    norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-    normalized_embeddings = embeddings / norm
-    valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-    similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+        # Model.
+        # Look up embeddings for inputs.
+        embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+        # Compute the softmax loss, using a sample of the negative labels each time.
+        loss = tf.reduce_mean(
+          tf.nn.sampled_softmax_loss(softmax_weights, softmax_biases, embed,
+                                     train_labels, num_sampled, vocabulary_size))
 
-num_steps = 100001
+        # Optimizer.
+        # Note: The optimizer will optimize the softmax_weights AND the embeddings.
+        # This is because the embeddings are defined as a variable quantity and the
+        # optimizer's `minimize` method will by default modify all variable quantities
+        # that contribute to the tensor it is passed.
+        # See docs on `tf.train.Optimizer.minimize()` for more details.
+        optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
 
-with tf.Session(graph=graph) as session:
-    tf.global_variables_initializer().run()
-    print('Initialized')
-    average_loss = 0
-    for step in range(num_steps):
-        batch_data, batch_labels = generate_batch(
-          batch_size, num_skips, skip_window)
-        feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
-        _, l = session.run([optimizer, loss], feed_dict=feed_dict)
-        average_loss += l
-        if step % 2000 == 0:
-            if step > 0:
-                average_loss = average_loss / 2000
-            # The average loss is an estimate of the loss over the last 2000 batches.
-            print('Average loss at step %d: %f' % (step, average_loss))
-            average_loss = 0
-        # note that this is expensive (~20% slowdown if computed every 500 steps)
-        if step % 10000 == 0:
-            sim = similarity.eval()
-            for i in range(valid_size):
-                valid_word = reverse_dictionary[valid_examples[i]]
-                top_k = 8 # number of nearest neighbors
-                nearest = (-sim[i, :]).argsort()[1:top_k+1]
-                log = 'Nearest to %s:' % valid_word
-                for k in range(top_k):
-                    close_word = reverse_dictionary[nearest[k]]
-                    log = '%s %s,' % (log, close_word)
-                print(log)
-    final_embeddings = normalized_embeddings.eval()
+        # Compute the similarity between minibatch examples and all embeddings.
+        # We use the cosine distance:
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+        normalized_embeddings = embeddings / norm
+        valid_embeddings = tf.nn.embedding_lookup(
+          normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
 
-num_points = 400
+    num_steps = 100001
 
-tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :])
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initialized')
+        average_loss = 0
+        for step in range(num_steps):
+            batch_data, batch_labels = generate_batch(
+              batch_size, num_skips, skip_window)
+            feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
+            _, l = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += l
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss = average_loss / 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step %d: %f' % (step, average_loss))
+                average_loss = 0
+            # note that this is expensive (~20% slowdown if computed every 500 steps)
+            if step % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8 # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = reverse_dictionary[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+        final_embeddings = normalized_embeddings.eval()
+        return final_embeddings
 
-def plot(embeddings, labels):
+
+def plot(embeddings, labels, is_skip = True):
     assert embeddings.shape[0] >= len(labels), 'More labels than embeddings'
-    pylab.figure(figsize=(15,15))  # in inches
+    plt.figure()  # in inches
     for i, label in enumerate(labels):
         x, y = embeddings[i,:]
-        pylab.scatter(x, y)
-        pylab.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
+        plt.scatter(x, y)
+        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points',
                        ha='right', va='bottom')
-    pylab.show()
 
-words = [reverse_dictionary[i] for i in range(1, num_points+1)]
-plot(two_d_embeddings, words)
+    if is_skip:
+        savename = 'embeding_skip.png'
+    else:
+        savename = 'embeding_cbow.png'
+
+    plt.savefig(savename)
+
+
+def run_skip_w2v():
+
+    final_embeddings = skip_word2vec()
+
+    num_points = 400
+
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :])
+
+    words = [reverse_dictionary[i] for i in range(1, num_points+1)]
+    plot(two_d_embeddings, words)
+
 
 # Ex 1: Implement cbow word2vec, insted of predicting a context word from a word vector, you predict a word from the sum of all the word vectors in its context.
 # Implement and evaluate a CBOW model trained on the text8 dataset.
+
+if __name__ == '__main__':
+    run_skip_w2v()
