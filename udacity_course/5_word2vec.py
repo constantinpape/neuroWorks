61aac04cee11bbaacc29a0b31d3c018b3cd6d611
Constantin Pape <c.pape@gmx.net>
2016-12-27 14:00:01 +0100

    Implementing cbpw

--- udacity_course/5_word2vec.py
+++ udacity_course/5_word2vec.py
@@ -66,13 +66,16 @@ print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10])
 del words  # Hint to reduce memory.
 
+print('data:', [reverse_dictionary[di] for di in data[:8]])
+
 data_index = 0
 
+# generating a batch for skip gram
 # batch size: number of examples in the batch
 # num_skips: number of times single word is repeated as a label
 # skip_window: length of window target for the word is chosen from
 # TODO does anything but num_skips = 2*skip_window make sense?
-def generate_batch(batch_size, num_skips, skip_window):
+def generate_batch_skip(batch_size, num_skips, skip_window):
     global data_index
     assert batch_size % num_skips == 0
     assert num_skips <= 2 * skip_window
@@ -95,15 +98,6 @@ def generate_batch(batch_size, num_skips, skip_window):
         buffer.append(data[data_index])
         data_index = (data_index + 1) % len(data)
     return batch, labels
-
-print('data:', [reverse_dictionary[di] for di in data[:8]])
-
-for num_skips, skip_window in [(2, 1), (4, 2)]:
-    data_index = 0
-    batch, labels = generate_batch(batch_size=8, num_skips=num_skips, skip_window=skip_window)
-    print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
-    print('    batch:', [reverse_dictionary[bi] for bi in batch])
-    print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
 
 
 def skip_word2vec():
@@ -168,7 +162,7 @@ def skip_word2vec():
         print('Initialized')
         average_loss = 0
         for step in range(num_steps):
-            batch_data, batch_labels = generate_batch(
+            batch_data, batch_labels = generate_batch_skip(
               batch_size, num_skips, skip_window)
             feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
             _, l = session.run([optimizer, loss], feed_dict=feed_dict)
@@ -214,6 +208,13 @@ def plot(embeddings, labels, is_skip = True):
 
 def run_skip_w2v():
 
+    for num_skips, skip_window in [(2, 1), (4, 2)]:
+        data_index = 0
+        batch, labels = generate_batch_skip(batch_size=8, num_skips=num_skips, skip_window=skip_window)
+        print('\nwith num_skips = %d and skip_window = %d:' % (num_skips, skip_window))
+        print('    batch:', [reverse_dictionary[bi] for bi in batch])
+        print('    labels:', [reverse_dictionary[li] for li in labels.reshape(8)])
+
     final_embeddings = skip_word2vec()
 
     num_points = 400
@@ -228,5 +229,130 @@ def run_skip_w2v():
 # Ex 1: Implement cbow word2vec, insted of predicting a context word from a word vector, you predict a word from the sum of all the word vectors in its context.
 # Implement and evaluate a CBOW model trained on the text8 dataset.
 
+
+# context_len: len of context around the word that should be predicted
+def generate_batch_cbow(batch_size, context_len):
+    global data_index
+
+    batch = np.zeros(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+
+    for i in range(batch_size):
+
+        index  = (data_index + i) % len(data)
+
+        labels[i] = data[index]
+
+        for j in range(1,context_len+1):
+            batch[i] += data[(index + j) % len(data)]
+            batch[i] += data[(index - j) % len(data)]
+
+    data_index = (data_index + batch_size) % len(data)
+
+    return batch, labels
+
+
+def cbow_word2vec():
+
+    batch_size = 128
+    embedding_size = 128 # Dimension of the embedding vector.
+    context_len = 4
+    # We pick a random validation set to sample nearest neighbors. here we limit the
+    # validation samples to the words that have a low numeric ID, which by
+    # construction are also the most frequent.
+    valid_size = 16 # Random set of words to evaluate similarity on.
+    valid_window = 100 # Only pick dev samples in the head of the distribution.
+    valid_examples = np.array(random.sample(range(valid_window), valid_size))
+    num_sampled = 64 # Number of negative examples to sample.
+
+    graph = tf.Graph()
+
+    with graph.as_default(), tf.device('/cpu:0'):
+
+        # Input data.
+        train_dataset = tf.placeholder(tf.int32, shape=[batch_size])
+        train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+        valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+        # Variables.
+        embeddings = tf.Variable(
+          tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+        softmax_weights = tf.Variable(
+          tf.truncated_normal([vocabulary_size, embedding_size],
+                               stddev=1.0 / math.sqrt(embedding_size)))
+        softmax_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+        # Model.
+        # Look up embeddings for inputs.
+        embed = tf.nn.embedding_lookup(embeddings, train_dataset)
+        # Compute the softmax loss, using a sample of the negative labels each time.
+        loss = tf.reduce_mean(
+          tf.nn.sampled_softmax_loss(softmax_weights, softmax_biases, embed,
+                                     train_labels, num_sampled, vocabulary_size))
+
+        # Optimizer.
+        # Note: The optimizer will optimize the softmax_weights AND the embeddings.
+        # This is because the embeddings are defined as a variable quantity and the
+        # optimizer's `minimize` method will by default modify all variable quantities
+        # that contribute to the tensor it is passed.
+        # See docs on `tf.train.Optimizer.minimize()` for more details.
+        optimizer = tf.train.AdagradOptimizer(1.0).minimize(loss)
+
+        # Compute the similarity between minibatch examples and all embeddings.
+        # We use the cosine distance:
+        norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+        normalized_embeddings = embeddings / norm
+        valid_embeddings = tf.nn.embedding_lookup(
+          normalized_embeddings, valid_dataset)
+        similarity = tf.matmul(valid_embeddings, tf.transpose(normalized_embeddings))
+
+    num_steps = 100001
+
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initialized')
+        average_loss = 0
+        for step in range(num_steps):
+            batch_data, batch_labels = generate_batch_cbow(
+              batch_size, num_skips, skip_window)
+            feed_dict = {train_dataset : batch_data, train_labels : batch_labels}
+            _, l = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += l
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss = average_loss / 2000
+                # The average loss is an estimate of the loss over the last 2000 batches.
+                print('Average loss at step %d: %f' % (step, average_loss))
+                average_loss = 0
+            # note that this is expensive (~20% slowdown if computed every 500 steps)
+            if step % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8 # number of nearest neighbors
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log = 'Nearest to %s:' % valid_word
+                    for k in range(top_k):
+                        close_word = reverse_dictionary[nearest[k]]
+                        log = '%s %s,' % (log, close_word)
+                    print(log)
+        final_embeddings = normalized_embeddings.eval()
+        return final_embeddings
+
+
+def run_cbow_w2v():
+    final_embeddings = cbow_word2vec()
+
+    num_points = 400
+
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    two_d_embeddings = tsne.fit_transform(final_embeddings[1:num_points+1, :])
+
+    words = [reverse_dictionary[i] for i in range(1, num_points+1)]
+    plot(two_d_embeddings, words)
+
+
 if __name__ == '__main__':
-    run_skip_w2v()
+    #print data[:10]
+    #run_skip_w2v()
+    run_cbow_w2v()
